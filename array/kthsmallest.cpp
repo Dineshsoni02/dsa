73508850aa698788b1727e1bef98b31eea28ec12
Dinesh kumar <dineshsoni8569@gmail.com>
2023-06-10 21:15:54 +0530

    solved

--- array/kthsmallest.cpp
+++ array/kthsmallest.cpp
@@ -1,50 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int sortArray(int arr[], int n, int k)
+int kthSmallest(int arr[], int n, int k)
 {
 
     for (int i = 0; i < n; i++)
     {
-        int temp = i;
 
         for (int j = i + 1; j < n; j++)
         {
             if (arr[i] > arr[j])
             {
-                temp = j;
+                int t = arr[i];
+                arr[i] = arr[j];
+                arr[j] = t;
             }
         }
-        if (temp != i)
-        {
-            int t = arr[i];
-            arr[i] = arr[temp];
-            arr[temp] = t;
-        }
     }
-}
-
-int kthSmallest(int arr[], int n, int k)
-{   
-    sortArray(arr,  n, k);
-
     return arr[k - 1];
 }
 
 int main()
 {
+    int arr[10]={5,-5,8,88,66,4,-55}, k, n;
+    // cout << "enter size of array:";
+    // cin >> n;
 
-    int arr[10], k, n;
-    cout << "enter size of array:";
-    cin >> n;
-
-    cout << "enter elements in array:";
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr[i];
-    }
+    // cout << "enter elements in array:";
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
     cout << "Enter kth smallest element to be returned: ";
     cin >> k;
- cout<<"Result:"<<kthSmallest(arr, n, k);
+    cout << "Result:" << kthSmallest(arr, 7, k);
     return 0;
 }
