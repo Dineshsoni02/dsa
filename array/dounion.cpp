6306aa00ccb826730ee837dae97a332018d25246
Dinesh kumar <dineshsoni8569@gmail.com>
2023-06-11 22:26:08 +0530

    intersection

--- array/dounion.cpp
+++ array/dounion.cpp
@@ -32,3 +32,31 @@ int main()
     doUnion(a, n, b, m);
     return 0;
 }
+
+
+//intersection
+/*
+
+class Solution {
+  public:
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        // Your code goes here
+       set<int> st1,st2;
+        
+        for(int i=0;i<n;i++){
+            st1.insert(a[i]);
+        }
+        for(int j=0;j<m;j++){
+            st2.insert(b[j]);
+        }
+       int ct=0;
+    
+       for(auto val:st1){
+           if(st2.count(val)) ct++;
+       }
+       return ct;
+    }
+};
+*/
