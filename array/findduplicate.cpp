789b12964ca8edc6b420f7c45addece71503f51f
Dinesh kumar <dineshsoni8569@gmail.com>
2023-06-23 18:44:19 +0530

    optimised

--- array/findduplicate.cpp
+++ array/findduplicate.cpp
@@ -5,18 +5,36 @@ int findDuplicate(int arr[], int n)
 {
 
     // brute force
+    /*
     int ans = 0;
-    for (int i = 0; i < n; i++)
+     for (int i = 0; i < n; i++)
+     {
+         for (int j = i + 1; i < n; i++)
+         {
+             if (arr[i] == arr[j])
+             {
+                 ans = arr[j];
+             }
+         }
+     }
+     return ans;
+    */
+    // optimised
+    int slow = arr[0];
+    int fast = arr[0];
+    do
     {
-        for (int j = i + 1; i < n; i++)
-        {
-            if (arr[i] == arr[j])
-            {
-                ans = arr[j];
-            }
-        }
+        slow = arr[slow];
+        fast = arr[arr[fast]];
+    } while (slow != fast);
+
+    fast = arr[0];
+    while (slow != fast)
+    {
+        slow = arr[slow];
+        fast = arr[fast];
     }
-    return ans;
+    return slow;
 }
 
 int main()
