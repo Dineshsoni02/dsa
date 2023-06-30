339f5e2f92cd2d1f9c2aca404427fa3b76b4c3a1
Dinesh kumar <dineshsoni8569@gmail.com>
2023-06-30 22:02:38 +0530

    next permutation

--- array/nextpermutation.cpp
+++ array/nextpermutation.cpp
@@ -3,25 +3,51 @@
 #include <vector>
 using namespace std;
 
-void nextPermutation(vector<int> &val)
+void nextPermutation(vector<int> &nums)
 {
-//using stl 😁
-    next_permutation(val.begin(), val.end());
+    // using stl 😁
+    //  next_permutation(nums.begin(), nums.end());
+    int ind = -1;
+    int n = nums.size();
+    for (int i = n - 2; i > 0; i--)
+    {
+        if (nums[i] < nums[i + 1])
+        {
+            ind = i;
+            break;
+        }
+    }
+    if (ind == -1)
+    {
+        reverse(nums.begin(), nums.end());
+    }
+    else
+    {
+        for (int j = n - 1; j > ind; j--)
+        {
+            if (nums[j] > nums[ind])
+            {
+                swap(nums[j], nums[ind]);
+                break;
+            }
+        }
+        reverse(nums.begin() + ind + 1, nums.end());
+    }
 }
 
 int main()
 {
-    vector<int> val;
+    vector<int> nums;
     int a;
-    cout<<"enter values: ";
+    cout << "enter values: ";
     for (int i = 0; i < 3; i++)
     {
         cin >> a;
-        val.push_back(a);
+        nums.push_back(a);
     }
-    nextPermutation(val);
+    nextPermutation(nums);
     cout << "ans: ";
-    for (auto ans : val)
+    for (auto ans : nums)
     {
         cout << ans << " ";
     }
