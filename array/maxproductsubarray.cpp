554937bee2ba70329da044afcc9c12df2a807dd1
Dinesh kumar <dineshsoni8569@gmail.com>
2023-07-12 17:22:04 +0530

    152. Maximum Product Subarray 2nd

--- array/maxproductsubarray.cpp
+++ array/maxproductsubarray.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include<climits>
+#include <climits>
 using namespace std;
 
 int maxProSubArr(int *arr, int n)
@@ -12,9 +12,27 @@ int maxProSubArr(int *arr, int n)
             pre = 1; // if pre becomes 0 assign pre=1 , same with suff
         if (suff == 0)
             suff = 1;
-        pre *= arr[i];                 // multiply pre from start elements
-        suff *= arr[n - i - 1];        // suff from last
+        pre *= arr[i];                  // multiply pre from start elements
+        suff *= arr[n - i - 1];         // suff from last
         ans = max(ans, max(pre, suff)); // return max of both and ans
+    }
+    return ans;
+}
+
+int maxProSubArr1(int *arr, int n)
+{
+    // using kadane's
+    int minV = arr[0], maxV = arr[0];
+    int ans = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < 0)
+        {
+            maxV = maxV + minV - (minV = maxV);
+        }
+        maxV = max(arr[i], arr[i] * maxV);
+        minV = min(arr[i], arr[i] * minV);
+        ans = max(maxV, ans);
     }
     return ans;
 }
